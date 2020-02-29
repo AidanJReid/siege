748d81af01edec2b32410ae14a642eb173211df6
Aidan Reid <aidan.reid@hotmail.com>
2020-02-29 14:33:48 +0000

    workaround for form

--- js/contact_me.js
+++ js/contact_me.js
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Crap. Sorry " + firstName + ", looks like my mail server isn't responding. Please hit me up on aidan.reid(at)hotmail.com instead!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
