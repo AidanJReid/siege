cb0375c1c44750ca7eb7eee3077dfcada706cfd6
Aidan Reid <aidan.reid@hotmail.com>
2020-03-11 15:36:46 +0000

    removed stretch option

--- js/tdrop.js
+++ js/tdrop.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 	$('.box').on('click', function () {
-		var animations = [ 'crawl', 'dangle', 'jump', 'stretch', 'flip' ];
+		var animations = [ 'crawl', 'dangle', 'jump', 'flip' ];
 		var box = this;
 		var animation = animations[Math.floor(Math.random() * 5)];
 
