4454f79e0cccc11b60a78910d0d3fcc88352d9a4
Aidan Reid <aidan.reid@hotmail.com>
2020-03-22 13:45:35 +0000

    updated w tooth value calc - using old bootstrap tags to render

--- js/interest.js
+++ js/interest.js
@@ -1,48 +1,67 @@
 // Listen for submit
+document.getElementById('inflation-form').addEventListener('submit', function(e){
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+    // Show Loader
+    document.getElementById('loading').style.display = 'block';
 
-document.getElementById('inflation-form').addEventListener('submit', calculateResults);
-
-// Calculate Results
-function calculateResults(e){
-    console.log("Working");
-    // UI variables
-    const amount = getElementById("amount");
-    const inflation = getElementById("inflation");
-    const years = getElementById("years");
-    const monthlyInflation = getElementById("monthly-inflation");
-    const totalValue = getElementById("total-value");
-    const totalInflation = getElementById("total-inflation");
-
-    const principal = parseFloat(amount.value);
-    const calculatedInflation = parseFloat(inflation.value) / 100 / 12;
-    const calculatedYears = parseFloat(years.value) * 12;
-
-    // Compute Monthly Payments
-    const x = Math.pow(1 + calculatedInflation, calculatedYears);
-    const monthly = (principal*x*calculatedInflation)/(x-1);
-
-    if(isFinite(monthly)){
-        monthlyInflation.value = monthly.toFixed(2);
-        totalValue.value = (monthly * calculatedYears).toFixed(2);
-        totalInflation.value = ((monthly * calculatedYears)-principal).toFixed(2);
-    } else {
-        showError("Please check your numbers");
-    }
+    setTimeout(calculateResults, 2000);
 
     e.preventDefault();
+});
+
+// Calculate Results
+function calculateResults(){
+  console.log('Calculating...');
+  // UI Vars
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-inflation');
+  const totalPayment = document.getElementById('total-value');
+  const totalInterest = document.getElementById('total-inflation');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+  } else {
+    showError('Check your numbers again numbnuts');
+  }
 }
 
 // Show Error
 function showError(error){
-    //Create div
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+    // Create a div
     const errorDiv = document.createElement('div');
 
     // Get elements
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
 
-    // Add Class
-    errorDiv.className = "alert alert-danger";
+    // Add class
+    errorDiv.className = 'alert alert-danger';
 
     // Create text node and append to div
     errorDiv.appendChild(document.createTextNode(error));
@@ -52,10 +71,9 @@ function showError(error){
 
     // Clear error after 3 seconds
     setTimeout(clearError, 3000);
-
 }
 
-// Clear Error
+// Clear error
 function clearError(){
-    document.querySelector('.alert').remove();
+  document.querySelector('.alert').remove();
 }
